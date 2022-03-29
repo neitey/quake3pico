aeb45adcd82e6d06aa0c9d64aaff5fa365d1e595
Simon <simonbrown77@googlemail.com>
2022-03-29 23:23:33 +0100

    Small changes following the merge from master
    - and also removed a couple of bits of my hacking

--- android/app/src/main/cpp/code/renderergl2/tr_glsl.c
+++ android/app/src/main/cpp/code/renderergl2/tr_glsl.c
@@ -337,9 +337,9 @@ static void GLSL_GetShaderHeader( GLenum shaderType, const GLchar *extra, char *
     }
 
     // HACK: use in main menu medium float precision (to prevent issue with missing models textures)
-//    if (Cvar_Get("r_uiFullScreen", "1", 0)->integer)
-//        Q_strcat(dest, size, "precision mediump float;\n");
-//    else
+    if (Cvar_Get("r_uiFullScreen", "1", 0)->integer)
+        Q_strcat(dest, size, "precision mediump float;\n");
+    else
         Q_strcat(dest, size, "precision highp float;\n");
 
 	if(shaderType == GL_VERTEX_SHADER)

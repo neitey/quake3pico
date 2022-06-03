08a227f088211e08c23591e6016f7dba0c877ca3
Lubos <tridosm@gmail.com>
2022-06-03 20:03:29 +0200

    OpenXR Pico extension to support controllers added

--- android/app/src/main/cpp/code/vr/vr_base.c
+++ android/app/src/main/cpp/code/vr/vr_base.c
@@ -13,10 +13,13 @@ static engine_t vr_engine;
 qboolean vr_initialized = qfalse;
 extern vr_clientinfo_t vr;
 
+#define XR_PICO_ANDROID_CONTROLLER_FUNCTION_EXT_ENABLE_EXTENSION_NAME "XR_PICO_android_controller_function_ext_enable"
+
 const char* const requiredExtensionNames[] = {
         XR_KHR_ANDROID_CREATE_INSTANCE_EXTENSION_NAME,
         XR_KHR_OPENGL_ES_ENABLE_EXTENSION_NAME,
-        XR_KHR_COMPOSITION_LAYER_CYLINDER_EXTENSION_NAME};
+        XR_KHR_COMPOSITION_LAYER_CYLINDER_EXTENSION_NAME,
+        XR_PICO_ANDROID_CONTROLLER_FUNCTION_EXT_ENABLE_EXTENSION_NAME};
 const uint32_t numRequiredExtensions =
         sizeof(requiredExtensionNames) / sizeof(requiredExtensionNames[0]);
 
